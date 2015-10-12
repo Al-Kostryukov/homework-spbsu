084b7fc5205657533a4ff67b302fd3c787855e78
Alexey Kostryukov <Al-Kostryukov@yandex.ru>
2015-10-13 01:07:11 +0300

    addOK is updated

--- BitwiseOperations_2/addOK/main.c
+++ BitwiseOperations_2/addOK/main.c
@@ -9,11 +9,6 @@ int addOK(int x, int y){
     int signx = x >> 31;
     int signy = y >> 31;
     int signsum = sum >> 31;
-    //return (~(signx^signy)+1)|(!(signx^signsum));
-    //if signx!=signy then ok, return 1.
-    //if signx!=signsum (and signy!=signsum) (складывали числа одного знака, а получили сумму другого знака) then return 0
-
-    //maybe another solution (unites both 'if') is !((signx^signsum)&(signy^signsum))
     return !((signx ^ signsum) & (signy ^ signsum));
 }
 
