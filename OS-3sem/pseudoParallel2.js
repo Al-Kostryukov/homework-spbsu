0dbe264e7427cac3abe1ba66660db8b179df5ed2
Alexey Kostryukov <Al-Kostryukov@yandex.ru>
2016-09-27 00:54:45 +0300

    added priority policy; this.policy now returns task to execute;

--- OS-3sem/pseudoParallel2.js
+++ OS-3sem/pseudoParallel2.js
@@ -1,6 +1,6 @@
 /*
  * You can run it: node pseudoParallel2.js
- * [if nodejs installed]
+ * [if nodejs v>=6.0.0 installed]
 */
 
 /*
@@ -50,23 +50,20 @@ Sheduler.prototype = {
 	},
 	run: function() {
 		while (this.tasks.length) {
-			this.policy(this.tasks);
-			this.removeDoneTasks();
-		};
+			let task = this.policy(this.tasks); //this.policy(tasks) returns most preferred for current policy task to execute
+
+			task.execute();
+			
+			if (task.state == 'Done') {
+				this.removeTask(task.id);
+			}
+		}
 	},
 	removeTask: function(id) {
 		for (let i = 0; i < this.tasks.length; i++) {			
 			if (this.tasks[i].id == id) {
 				this.tasks.splice(i, 1); //removing task from tasks array
-				i--;
-			}
-		}
-	},
-	removeDoneTasks: function() {
-		for (let i = 0; i < this.tasks.length; i++) {			
-			if (this.tasks[i].state == 'Done') {
-				this.removeTask(this.tasks[i].id);
-				i--;
+				return;
 			}
 		}
 	},
@@ -82,8 +79,6 @@ Sheduler.prototype = {
 			//tasks is one task here
 			tasks.id = this.getNextTaskId();
 		}
-
-		return tasks;
 	}
 }
 
@@ -92,7 +87,7 @@ Sheduler.prototype = {
 
 /*
  * Policy constructor
- *
+ * 
 */
 
 let Policy = function(type) {
@@ -105,14 +100,16 @@ let Policy = function(type) {
 			break;
 		case 'guaranteed':
 			return this.guaranteed;
+			break;
+		case 'priority':
+			return this.priority;
+			break;
 	}
 }
 
 Policy.prototype = {
 	cycle: function(tasks) {
-		for (let i = 0; i < tasks.length; i++) {	
-			tasks[i].execute();
-		}
+		return tasks[0];
 	},
 	timePriority: function(tasks) {
 		//not implemented
@@ -121,7 +118,17 @@ Policy.prototype = {
 		//not implemented.
 	},
 	priority: function(tasks) {
-		//sort tasks by priority, then execute
+		let maxPriority = tasks[0].priority,
+			task_i = 0;
+		
+		for (let i = 1; i < tasks.length; i++) {	
+			if (tasks[i].priority > maxPriority) {
+				maxPriority = tasks[i].priority,
+				task_i = i;
+			}
+		}
+
+		return tasks[task_i];	
 	}
 }
 
@@ -137,15 +144,17 @@ Policy.prototype = {
 
 let Task = function(opts) {
 	this.func = opts.func;
-	this.state = 'Created'; //I know useing strings is bad, but
-	this.priority = opts.priority || 1; //for future priority function priority 
+	this.state = 'Created';
+	this.priority = opts.priority || 1; //task executes first if it has HIGHER priority (&& policyType == 'priority')
 }
 
 Task.prototype = {
 	execute: function() {
-		this.state = 'Executing';
-		this.func();
-		this.state = 'Done';
+		if (this.state != 'Done') {
+			this.state = 'Executing';
+			this.func();
+			this.state = 'Done';
+		}
 	}
 }
 
@@ -154,59 +163,8 @@ Task.prototype = {
 
 
 
-/*
- * Our tasks
- *
-*/
-
-function asyncBlockingOperaton(func, callback) {
-	console.log("asyncBlockingOperaton. It will be executed later, on next loop tick");
-	sheduler.addTasks(
-		new Task({
-			func: function() {
-				func();
-				callback();
-			}
-		})
-	)
-}
-
-function blockingOperaton(func, callback) {
-	console.log("blockingOperaton. It will be executed NOW. So you need to wait :(");
-	func();
-	callback();
-}
-
-let task1 = new Task({
-	func: () => {
-		console.log('hello [from task1]');
-		asyncBlockingOperaton(function() {
-			sleep(5000)
-		}, function() {
-			console.log('world [from task1]');
-		});
-	}
-})
-
-let task2 = new Task({
-	func: () => {
-		console.log('hello [from task2]');
-		sleep(2000);
-		console.log('world [from task2]');
-	}
-})
 
 
-let task3 = new Task({
-	func: () => {
-		console.log('hello [from task3]');
-		blockingOperaton(function() {
-			sleep(5000)
-		}, function() {
-			console.log('world [from task3]');
-		});
-	}
-})
 
 
 
@@ -216,9 +174,159 @@ let task3 = new Task({
  *
 */
 
-let sheduler = new Sheduler({
+
+
+function asyncOperaton(func, callback) {
+	console.log("asyncOperaton. It will be executed later, after executing all existing tasks at the moment");
+	curSheduler.addTasks(
+		new Task({
+			func: function() {
+				func();
+				callback();
+			}
+		})
+	);
+}
+
+
+function asyncOperatonWithHighPriority(func, callback) {
+	console.log("asyncOperatonWithHighPriority. It will be executed now");
+	curSheduler.addTasks(
+		new Task({
+			func: function() {
+				func();
+				callback();
+			},
+			priority: 50
+		})
+	);
+}
+
+function syncOperaton(func, callback) {
+	console.log("syncOperaton. It will be executed NOW. So you need to wait :(");
+	func();
+	callback();
+}
+
+
+/*
+ * 1'st sheduler
+ *
+*/
+
+console.log("SHEDULER 1");
+
+/*
+ * Our tasks
+ *
+*/
+
+
+let task1 = new Task({
+	func: () => {
+		console.log('hello [from task1]');
+		asyncOperaton(function() {
+			sleep(10000)
+		}, function() {
+			console.log('world [from task1]');
+		});
+	}
+})
+
+let task2 = new Task({
+	func: () => {
+		console.log('hello [from task2]');
+		sleep(10000);
+		console.log('world [from task2]');
+	}
+})
+
+let task3 = new Task({
+	func: () => {
+		console.log('hello [from task3]');
+		syncOperaton(function() {
+			sleep(10000)
+		}, function() {
+			console.log('world [from task3]');
+		});
+	}
+})
+
+
+let sheduler1 = new Sheduler({
 	policyType: 'cycle'
 });
 
-sheduler.addTasks([task1, task2, task3]);
-sheduler.run();
+let curSheduler = sheduler1;
+
+sheduler1.addTasks([task1, task2, task3]);
+sheduler1.run();
+
+
+
+
+/*
+ * 2'nd sheduler
+ *
+*/
+
+console.log("\nSHEDULER 2");
+
+/*
+ * Our tasks
+ *
+*/
+
+let task4 = new Task({
+	func: () => {
+		console.log('hello [from task4]');
+		asyncOperaton(function() {
+			sleep(10000)
+		}, function() {
+			console.log('world [from task4]');
+		});
+	},
+	priority: 5
+})
+
+let task5 = new Task({
+	func: () => {
+		console.log('hello [from task5]');
+		sleep(10000);
+		console.log('world [from task5]');
+	},
+	priority: 10
+})
+
+let task6 = new Task({
+	func: () => {
+		console.log('hello [from task6]');
+		syncOperaton(function() {
+			sleep(10000)
+		}, function() {
+			console.log('world [from task6]');
+		});
+	},
+	priority: 15
+})
+
+let task7 = new Task({
+	func: () => {
+		console.log('hello [from task7]');
+		asyncOperatonWithHighPriority(function() {
+			sleep(10000)
+		}, function() {
+			console.log('world [from task7]');
+		});
+	},
+	priority: 12
+})
+
+let sheduler2 = new Sheduler({
+	policyType: 'priority'
+});
+
+curSheduler = sheduler2; 
+
+sheduler2.addTasks([task4, task5, task6, task7]);
+sheduler2.run();
