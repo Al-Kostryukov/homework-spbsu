172fa7bc350b43394843f9df10759b4cc6c6337b
Alexey Kostryukov <Al-Kostryukov@yandex.ru>
2018-04-17 03:47:56 +0300

    small improve for algo 3

--- 6sem-fl-competitions/bottomup-improved.js
+++ 6sem-fl-competitions/bottomup-improved.js
@@ -1,11 +1,131 @@
-const Helper = require('./helper.js');
-const RFAGrammar = require('./rfa-grammar.js');
+class RFAGrammar {
+    //@param grammarAsString <string>
+    constructor(rfaGrammarAsString) {
+        this.rfa = this.parse(rfaGrammarAsString); //reversed: A -> BC will be this.productions['BC'] = 'A'
+    }
+
+    //@param rfaGrammarAsString <string>
+    parse(rfaGrammarAsString) {
+        let rfa = {
+                startStates: new Map(),
+                nonTermToFinalStates: new Map(),
+                finalStatesToNonTerm: new Map(),
+                graph: [],
+                outLabels: []
+            },
+            lines = rfaGrammarAsString.split(/\r?\n/),
+            regexpLabel = /(\d+).*label\s*=\s*"(.+?)"/,
+            regexpFinalState = /shape\s*=\s*"doublecircle"/,
+            regexpStartState = /color\s*=\s*"green"/;
+
+        for (let line of lines) {
+            if (line.length > 0) {
+                if (line.indexOf('->') > -1) {
+                    let [left, right] = line.split('->'),
+                        vertex1 = parseInt(left),
+                        rightMatched = right.match(regexpLabel),
+                        vertex2 = parseInt(rightMatched[1]),
+                        label = rightMatched[2];
+
+                    this.addEdge(vertex1, vertex2, label, rfa);
+                } else if (/\d+\[/.test(line)) {
+                    let matched = line.match(regexpLabel),
+                        vertex = parseInt(matched[1]),
+                        label = matched[2];
+
+                    if (regexpFinalState.test(line)) {
+                        if (rfa.nonTermToFinalStates.has(label)) {
+                            rfa.nonTermToFinalStates.get(label).add(vertex);
+                        } else {
+                            rfa.nonTermToFinalStates.set(label, new Set([vertex]));
+                        }
+
+                        if (rfa.finalStatesToNonTerm.has(vertex)) {
+                            rfa.finalStatesToNonTerm.get(vertex).push(label);
+                        } else {
+                            rfa.finalStatesToNonTerm[vertex] = [label];
+                        }
+                    }
+                    if (regexpStartState.test(line)) {
+                        if (rfa.startStates.hasOwnProperty(label)) {
+                            rfa.startStates[label].push(vertex);
+                        } else {
+                            rfa.startStates[label] = [vertex];
+                        }
+                    }
+                }
+            }
+        }
+
+        return rfa;
+    }
+
+    addEdge(vertex1, vertex2, label, rfa) {
+        if (!rfa) {
+            rfa = this.rfa;
+        }
+
+        if (rfa.outLabels[vertex1] == null) {
+            rfa.outLabels[vertex1] = new Set([label]);
+        } else {
+            rfa.outLabels[vertex1].add(label);
+        }
+
+        if (rfa.graph[vertex1] === undefined) {
+            rfa.graph[vertex1] = [[vertex2, [label]]]
+        } else {
+            let outForVertex1Labels = Helper.findArrayByFirstElem(rfa.graph[vertex1], vertex2);
+            if (outForVertex1Labels == null) {
+                rfa.graph[vertex1].push([vertex2, [label]])
+            } else if (outForVertex1Labels[1].indexOf(label) < 0){
+                outForVertex1Labels[1].push(label);
+            }
+        }
+    }
+
+    static isNonTerminal(str) {
+        return isNaN(str[0] * 1) && str[0] == str[0].toUpperCase()
+    }
+}
+
+
+
+class Helper {
+    static hasIntersectionSetAndSet(set1, set2) {
+        for (let el of set1) {
+            if (set2.has(el)) {
+                return true;
+            }
+        }
+
+        return false
+    }
+
+    static hasIntersectionArrayAndSet2(arr, set) {
+        for (let i = 0; i < arr.length; i++) {
+            if (set.has(arr[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static findArrayByFirstElem(arr, el) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i][0] == el) return arr[i];
+        }
+
+        return null;
+    }
+}
 
 
 class Graph {
   //@param graphAsString <string>
   constructor(graphAsString) {
     this.graphStructure = [];
+    this.outLabels = [];
     this.parse(graphAsString);
   }
 
@@ -27,24 +147,26 @@ class Graph {
   }
 
   addEdge(vertex1, vertex2, label) {
-    if (this.graphStructure[vertex1] == null) {
-      this.graphStructure[vertex1] = new Map();
-    }
+      if (this.outLabels[vertex1] == null) {
+          this.outLabels[vertex1] = new Set([label]);
+          this.graphStructure[vertex1] = new Map();
+      } else {
+          this.outLabels[vertex1].add(label);
+      }
 
-
-    let pushed = false;
+    //let pushed = false;
 
 
     let outForVertex1Labels = this.graphStructure[vertex1].get(vertex2);
     if (outForVertex1Labels == null) {
       this.graphStructure[vertex1].set(vertex2, new Set([label]));
-      pushed = true;
+      return true;
     } else if (!outForVertex1Labels.has(label)) {
       outForVertex1Labels.add(label);
-      pushed = true;
+      return true;
     }
 
-    return pushed;
+    return false;
   }
 }
 
@@ -102,25 +224,30 @@ module.exports = class BottomUpSolver {
     let smthChanged = false;
     const graphStructure = graph.graphStructure,
         workingPairs = [startPair],
-        milledPairs = [startPair];
+        milledPairs = new Set([startPair[0] * 1e12 + startPair[1]]);
 
     while (workingPairs.length) {
       let [rfaVertex, graphVertex] = workingPairs.pop();
 
-      if (rfa.finalStatesToNonTerm.hasOwnProperty(rfaVertex) && rfa.finalStatesToNonTerm[rfaVertex].indexOf(nonTerminal) > -1) {
+      if (rfa.finalStatesToNonTerm.hasOwnProperty(rfaVertex) &&
+          rfa.finalStatesToNonTerm[rfaVertex].indexOf(nonTerminal) > -1) {
           smthChanged |= graph.addEdge(startPair[1], graphVertex, nonTerminal);
       }
 
 
-      if (rfa.graph[rfaVertex] != null && graphStructure[graphVertex] != null) {
+      if (rfa.graph[rfaVertex] != null && graphStructure[graphVertex] != null &&
+          Helper.hasIntersectionSetAndSet(rfa.outLabels[rfaVertex], graph.outLabels[graphVertex])) {
         for (let outRfaVertexAndLabels of rfa.graph[rfaVertex]) {
           for (let outGraphVertexAndLabels of graphStructure[graphVertex]) {
             //intersect
             let i = Helper.hasIntersectionArrayAndSet2(outRfaVertexAndLabels[1], outGraphVertexAndLabels[1]);
-            let potentialToAdd = [outRfaVertexAndLabels[0], outGraphVertexAndLabels[0]];
-            if (i && !Helper.inArray2(milledPairs, potentialToAdd)) {
-              workingPairs.push(potentialToAdd);
-              milledPairs.push(potentialToAdd);
+            if (i) {
+                let potentialToAdd = [outRfaVertexAndLabels[0], outGraphVertexAndLabels[0]];
+                let key = potentialToAdd[0] * 1e12 + potentialToAdd[1];
+                if (!milledPairs.has(key)) {
+                    workingPairs.push(potentialToAdd);
+                    milledPairs.add(key);
+                }
             }
           }
         }
